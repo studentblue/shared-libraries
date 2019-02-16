7bfb9e37b1902707eab8e1aa2bc82f38da37946e
studentblue <e9651415@student.tuwien.ac.at>
2019-02-16 21:29:52 +0100

    Port and Debug Code

--- src/at/pii/jenkins_cpsiot_2018/sandbox/PortusApi.groovy
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/PortusApi.groovy
@@ -1,6 +1,8 @@
 package at.pii.jenkins_cpsiot_2018.sandbox
 
 import groovy.json.*
+import jenkins.plugins.http_request.*
+
 //import groovy.json.JsonSlurperClassic
 
 class PortusApi implements Serializable
