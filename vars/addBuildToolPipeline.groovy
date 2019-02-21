d70d48b9b24becb14f71832f2612e8ceca66c5ea
studentblue <e9651415@student.tuwien.ac.at>
2019-02-21 15:21:44 +0100

    Better Organization Debug

--- vars/addBuildToolPipeline.groovy
+++ vars/addBuildToolPipeline.groovy
@@ -30,6 +30,7 @@ def call( environment, currentBuild )
 						{
 							script
 							{
+								Log.addEntry(constants.ERROR, constants.ACTION_CREATED, "Test" )
 								//DockerHub.init(environment.AddBuildTool.DockerHub.repo, environment.AddBuildTool.DockerHub.tag)
 								//dockerHub.print()
 								//jenkinsBuildApi.init( currentBuild )
