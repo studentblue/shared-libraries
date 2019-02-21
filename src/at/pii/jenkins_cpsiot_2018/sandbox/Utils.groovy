92269187483dcec7ce967e61d9e576e4188fefa7
studentblue <e9651415@student.tuwien.ac.at>
2019-02-21 18:47:19 +0100

    Better Organization Debug

--- src/at/pii/jenkins_cpsiot_2018/sandbox/Utils.groovy
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/Utils.groovy
@@ -2,10 +2,10 @@ package at.pii.jenkins_cpsiot_2018.sandbox
 
 import groovy.json.*
 
-def Constants = new Constants()
-
 def httpRequestWithPlugin(url, mode, headers = [], body = "")
-{	
+{
+	def Constants = new Constants()
+	
 	def response = httpRequest httpMode: mode, url: url, acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', customHeaders: headers, requestBody: body
 	
 	if( mode.equals(Constants.HTTP_MODE_GET) && response.status == Constants.HTTP_RESPONSE_OK )
