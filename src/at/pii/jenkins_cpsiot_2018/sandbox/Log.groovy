3493e413acb672c280992963a3664a9a4336fc8e
studentblue <e9651415@student.tuwien.ac.at>
2019-02-21 16:41:49 +0100

    Better Organization Debug

--- src/at/pii/jenkins_cpsiot_2018/sandbox/Log.groovy
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/Log.groovy
@@ -12,13 +12,13 @@ class Log
 	
 	def addEntry(scope, action, message )
 	{
-		if( scope == Constants.ERROR )
+		if( scope == at.pii.jenkins_cpsiot_2018.sandbox.Constants.ERROR )
 		{
 			errors ++
 			log.add(ERROR + action + message )
 		}
 		
-		if( scope == Constants.LOG )
+		if( scope == at.pii.jenkins_cpsiot_2018.sandbox.Constants.LOG )
 		{
 			log.add(LOG + action + message )
 		}
