2582161469f4df5ea2c466ee6419edfeff074de6
studentblue <e9651415@student.tuwien.ac.at>
2019-02-21 19:02:47 +0100

    Better Organization Debug

--- src/at/pii/jenkins_cpsiot_2018/sandbox/Utils.groovy
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/Utils.groovy
@@ -2,17 +2,35 @@ package at.pii.jenkins_cpsiot_2018.sandbox
 
 import groovy.json.*
 
+//~ def httpRequestWithPlugin(url, mode, headers = [], body = "")
+//~ {
+	//~ def Constants = new Constants()
+	
+	//~ def response = httpRequest httpMode: mode, url: url, acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', customHeaders: headers, requestBody: body
+	
+	//~ if( mode.equals(Constants.HTTP_MODE_GET) && response.status == Constants.HTTP_RESPONSE_OK )
+		//~ return response
+	
+	//~ if( mode.equals(Constants.HTTP_MODE_POST) && response.status == Constants.HTTP_RESPONSE_CREATED )
+		//~ return response
+
+
+	//~ return false
+//~ }
+
+//~ return this
+
 def httpRequestWithPlugin(url, mode, headers = [], body = "")
 {
 	def Constants = new Constants()
 	
-	def response = httpRequest httpMode: mode, url: url, acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', customHeaders: headers, requestBody: body
+	//~ def response = httpRequest httpMode: mode, url: url, acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', customHeaders: headers, requestBody: body
 	
-	if( mode.equals(Constants.HTTP_MODE_GET) && response.status == Constants.HTTP_RESPONSE_OK )
-		return response
+	//~ if( mode.equals(Constants.HTTP_MODE_GET) && response.status == Constants.HTTP_RESPONSE_OK )
+		//~ return response
 	
-	if( mode.equals(Constants.HTTP_MODE_POST) && response.status == Constants.HTTP_RESPONSE_CREATED )
-		return response
+	//~ if( mode.equals(Constants.HTTP_MODE_POST) && response.status == Constants.HTTP_RESPONSE_CREATED )
+		//~ return response
 
 
 	return false
