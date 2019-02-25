e741762477c2507473e59b31414853785a3cfbdd
studentblue <e9651415@student.tuwien.ac.at>
2019-02-25 04:09:22 +0100

    Call PushImage Debug

--- src/at/pii/jenkins_cpsiot_2018/sandbox/addBuildToolHelpers.groovy
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/addBuildToolHelpers.groovy
@@ -290,7 +290,14 @@ class addBuildToolHelpers
 		user = user.replace(/[\W_]*$/, "")
 		
 		if( ! digest )
-			return Constants.DEFAULT_NAMESPACE_PREFIX + Constants.UNKNOWN_ARCH_OS + "-" + user
+		{
+			def manifest = DockerHub.getManifests()
+			
+			if( manifest.schemaVersion == 1)
+				return Constants.DEFAULT_NAMESPACE_PREFIX + "_" + manifest.architecture + "-" + user
+			else
+				return Constants.DEFAULT_NAMESPACE_PREFIX + Constants.UNKNOWN_ARCH_OS + "-" + user
+		}
 		
 		//DEFAULT_NAMESPACE_PREFIX
 		def defaultNameSpace = Constants.DEFAULT_NAMESPACE_PREFIX
