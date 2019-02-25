5c28f5a1e437cae2cbaca06aff0fef820f4feb24
studentblue <e9651415@student.tuwien.ac.at>
2019-02-25 04:57:25 +0100

    Call PushImage Debug

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,28 +1,23 @@
 def call( Map input )
 {	
-	//echo "${input.portusCredentials}"
 	
-	//pull image docker hub
 	
-	sh "docker pull ${input.DockerHubImage} "
+	//sh "docker pull ${input.DockerHubImage} "
 	
-	//echo "${input.DockerHubImage}"
-
+	echo "${input.DockerHubImage}"
+	/*
 	docker.withRegistry("${input.portusRepo}", "${input.portusCredentials}")
 	{
-		//customImage = docker.build("${env.DOCKER_USER_NAMESPACE}/${MY_SQL_SERVER_IMAGE_NAME}-${params.Build}-${params.Architecture}", "$WORKSPACE/database_scripts")
-		//customImage.push("latest")
-		//customImage.push("${env.BUILD_NUMBER}")
-		// sh " cd database_scripts && cat Dockerfile"
-		//echo "Push ${input.imageName.image}:${input.imageName.tag}"
+		
 		
 		sh "docker tag ${input.DockerHubImage} ${input.imageName.portusRepo}/${input.imageName.image}:${input.imageName.tag}"
 		sh "docker push ${input.imageName.portusRepo}/${input.imageName.image}:${input.imageName.tag}"
-		//echo "${input.imageName.portusRepo}/${input.imageName.image}:${input.imageName.tag}"
+		
 	}
 	
 	sh "docker rmi ${input.DockerHubImage} "
 	sh "docker rmi ${input.imageName.portusRepo}/${input.imageName.image}:${input.imageName.tag} "
+	*/
 }
 	
 //println DockerHub.getLog().printLog()
