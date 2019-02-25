d5bca0f0e0ae968b2fbadd4527281124cc0bd942
studentblue <e9651415@student.tuwien.ac.at>
2019-02-25 01:45:31 +0100

    Call PushImage Debug

--- vars/addBuildToolPipeline.groovy
+++ vars/addBuildToolPipeline.groovy
@@ -112,7 +112,7 @@ def call( environment, currentBuild )
 						input(id: "Push_Image", message: "Push as \""+image+"\"", ok: 'PUSH')
 						//AddBuildToolHelpers.pushImage(image)
 						
-						pushImage 
+						pushImage DockerHubImage: "Test", imageName: "Test", portusCredentials: "portus-user-creds"
 						
 					}
 					
