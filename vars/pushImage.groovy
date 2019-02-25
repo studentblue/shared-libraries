f3009451f7f2a3cbde61692b6308132729d1ccf0
studentblue <e9651415@student.tuwien.ac.at>
2019-02-25 01:29:14 +0100

    Call PushImage Debug

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,12 +1,12 @@
-def call(String DockerHubImage = "", String imageName = "", String portusCredentials = "portus-user-creds" )
+def call( input = ["DockerHubImage": "", "imageName" = "", "portusCredentials" = "portus-user-creds"] )
 {	
-	docker.withRegistry("https://${env.DOCKER_REPO}", "${portusCredentials}")
+	docker.withRegistry("https://${env.DOCKER_REPO}", "${input.portusCredentials}")
 	{
 		//customImage = docker.build("${env.DOCKER_USER_NAMESPACE}/${MY_SQL_SERVER_IMAGE_NAME}-${params.Build}-${params.Architecture}", "$WORKSPACE/database_scripts")
 		//customImage.push("latest")
 		//customImage.push("${env.BUILD_NUMBER}")
 		// sh " cd database_scripts && cat Dockerfile"
-		echo "Push ${imageName}"
+		echo "Push ${input.imageName}"
 	}
 }
 	
