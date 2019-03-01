853553ebcbb972a748c6438758523288041a27c7
studentblue <e9651415@student.tuwien.ac.at>
2019-03-01 04:24:21 +0100

    Alpha Test 3

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -2,22 +2,22 @@ def call( Map input )
 {	
 	
 	
-	//sh "docker pull ${input.DockerHubImage} "
+	sh "docker pull ${input.DockerHubImage}"
 	
-	echo "${input.DockerHubImage}"
-	echo "${input.portus.image.name}:${input.portus.repo.tag}"
+	//echo "${input.DockerHubImage}"
+	//echo "${input.portus.image.name}:${input.portus.repo.tag}"
 
 	docker.withRegistry("${input.portusRepo}", "${input.portusCredentials}")
 	{
 		
 		
-		sh "docker tag ${input.DockerHubImage} "${input.portus.image.name}:${input.portus.repo.tag}"
-		sh "docker push "${input.portus.image.name}:${input.portus.repo.tag}"
+		sh "docker tag ${input.DockerHubImage} ${input.portus.image.name}:${input.portus.repo.tag}"
+		sh "docker push ${input.portus.image.name}:${input.portus.repo.tag}"
 		
 	}
 	
 	sh "docker rmi ${input.DockerHubImage} "
-	sh "docker rmi "${input.portus.image.name}:${input.portus.repo.tag}"
+	sh "docker rmi ${input.portus.image.name}:${input.portus.repo.tag}"
 
 }
 	
