cb565fd5af395af7cee64f10a94f25d6caf07a82
studentblue <e9651415@student.tuwien.ac.at>
2019-03-06 00:25:07 +0100

    added BuildArrowHeadServerStackHelpers Pipeline and Helpers Start

---
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/buildArrowHeadServerStackHelpers.groovy
@@ -0,0 +1,44 @@
+package at.pii.jenkins_cpsiot_2018.sandbox;
+
+import groovy.json.*;
+import org.boon.Boon;
+
+//import groovy.json.JsonSlurperClassic
+
+class buildArrowHeadServerStackHelpers
+{
+
+	def log;
+	
+	def input;
+	
+	def Constants;
+	
+	def utils;
+	
+	def init( inputParameter, Constants  )
+	{
+		input = new JsonSlurperClassic().parseText(inputParameter)
+		
+		this.Constants = Constants		
+		
+		log = new Log()
+		log.init(Constants)
+		
+		utils = new Utils()
+		
+		log.addEntry(Constants.LOG, Constants.ACTION_LOG_START, "buildArrowHeadServerStackHelpers init" )
+		
+		checkInput()
+	}
+	
+	def checkInput()
+	{
+		log.addEntry(Constants.LOG, Constants.ACTION_CHECK, "Input Check ran" )
+	}
+	
+	def getParameter()
+	{
+		return input
+	}
+}
---
+++ vars/buildArrowHeadServerStackDemoPipeline.groovy
@@ -0,0 +1,61 @@
+def call( environment, currentBuild, parameter )
+{
+	
+	Constants = new at.pii.jenkins_cpsiot_2018.sandbox.Constants()
+	BuildArrowHeadServerStackHelpers = new at.pii.jenkins_cpsiot_2018.sandbox.buildArrowHeadServerStackHelpers()
+
+	pipeline
+	{
+		agent any
+		stages
+		{
+		
+			stage("init")
+			{
+				steps
+				{
+					script
+					{
+						BuildArrowHeadServerStackHelpers.init(parameter, Constants)
+						
+						println BuildArrowHeadServerStackHelpers.getParameter()
+					}
+				}
+			}
+		}
+		
+		post
+		{
+			always
+			{
+				echo 'Runs always'
+				script
+				{
+					if( BuildArrowHeadServerStackHelpers.getLog() )
+						println BuildArrowHeadServerStackHelpers.getLog().printLog()
+				}
+			}
+	
+			success
+			{
+				echo 'This will run only if successful'
+			}
+			
+			failure
+			{
+				echo 'This will run only if failed'
+			}
+			
+			unstable
+			{
+				echo 'This will run only if the run was marked as unstable'
+			}
+			
+			changed
+			{
+				echo 'This will run only if the state of the Pipeline has changed'
+				echo 'For example, the Pipeline was previously failing but is now successful'
+			}
+		}
+	}
+}	
