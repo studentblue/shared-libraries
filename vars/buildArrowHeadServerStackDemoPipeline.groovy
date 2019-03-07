a9fa44abb6bd782a73a68d7c8992643a31fa7b85
studentblue <e9651415@student.tuwien.ac.at>
2019-03-07 16:15:21 +0100

    added switch ArrowHead 3/4 and git select

--- vars/buildArrowHeadServerStackDemoPipeline.groovy
+++ vars/buildArrowHeadServerStackDemoPipeline.groovy
@@ -241,6 +241,9 @@ def call( environment, currentBuild, parameter )
 												
 												if( BuildArrowHeadServerStackHelpers.isArrowHead4() )
 												{
+													sh rm "target/config/default.conf"
+													sh rm "target/config/app.properties"
+													sh rm "target/config/log4j.properties"
 													writeFile file: 'target/config/app.conf', text: BuildArrowHeadServerStackHelpers.generateAppProperties4(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
 													println "is Arrowhead 4"
 												}
