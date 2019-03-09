ed7c57bbbf43ad0845b52dc3969277995d87fd8b
studentblue <e9651415@student.tuwien.ac.at>
2019-03-09 01:52:16 +0100

    build server stack version 2

---
+++ src/at/pii/jenkins_cpsiot_2018/sandbox/buildArrowHeadServerStackHelpers2.groovy
@@ -0,0 +1,510 @@
+package at.pii.jenkins_cpsiot_2018.sandbox;
+
+import groovy.json.*;
+import org.boon.Boon;
+
+//import groovy.json.JsonSlurperClassic
+
+class buildArrowHeadServerStackHelpers2
+{
+
+	def log;
+	
+	def input;
+	
+	def Constants;
+	
+	def utils;
+	
+	def environment
+	
+	def PortusApi
+	
+	def stdCloudTeam
+	
+	def JenkinsApi
+	
+	def init( inputParameter, Constants, environment, PortusApi, JenkinsApi  )
+	{
+		input = new JsonSlurperClassic().parseText(inputParameter)
+		
+		this.Constants = Constants
+		
+		this.environment = environment
+		
+		this.PortusApi = PortusApi
+		
+		this.JenkinsApi = JenkinsApi
+		
+		stdCloudTeam = environment.PORTUS_STD_CLOUD_TEAM
+		
+		log = new Log()
+		log.init(Constants)
+		
+		utils = new Utils()
+		
+		log.addEntry(Constants.LOG, Constants.ACTION_LOG_START, "buildArrowHeadServerStackHelpers init" )
+		
+		checkInput()
+	}
+	
+	def checkInput()
+	{
+		if( ! input.NameSpace.cloud )
+			log.addEntry(Constants.ERROR, Constants.ACTION_CHECK, "Cloud name not found" )
+		
+		log.addEntry(Constants.LOG, Constants.ACTION_CHECK, "Input Check ran" )
+	}
+	
+	def getParameter()
+	{
+		return input
+	}
+	
+	def checkCompileDockerHub()
+	{
+		return input.Compile.image.fromDockerHub
+	}
+	
+	def getDockerCompileImage()
+	{
+		if( ! checkCompileDockerHub() )
+		{			
+			def registry = environment.REPO_URL.split('//')[1]
+			
+			return registry +"/"+input.Compile.image.name
+		}
+		else
+		{
+			def image = ""
+			
+			if( input.Compile.image.namespace )
+				image += input.Compile.image.namespace
+			
+			if( input.Compile.image.repo && image)
+				image += "/" + input.Compile.image.namespace
+			
+			if( input.Compile.image.tag )
+			{
+				if( input.Compile.image.tag.contains(":") )
+					image += input.Compile.image.tag
+				else
+					image += ":" + input.Compile.image.tag
+			}
+			
+			return image
+		}
+	}
+	
+	def getContainerCompileArgs()
+	{
+		return input.Compile.imageArgs
+	}
+	
+	def getCompileCommand()
+	{
+		return input.Compile.command + " " + input.Compile.args
+	}
+	
+	def checkGenerateDBScript(image)
+	{
+		if( image.initDBScript == true )
+			return image.initDBScript
+		
+		return false
+	}
+	
+	def checkInputDBScript(image)
+	{
+		if( image.initDBScript.initDBScriptInput )
+			return true
+		else
+			return false
+	}
+	
+	def checkScriptPathDBScript(image)
+	{
+		if( image.initDBScript && ( image.initDBScript != true ) && image.initDBScript.initDBScriptPath )
+			return true
+		else
+			return false
+	}
+	
+	def checkIfDB()
+	{
+		def db = false
+		input.Images.each
+		{
+			image ->
+				if( image.database == true)
+				{
+					log.addEntry(Constants.LOG, Constants.ACTION_CHECK, "Database Image found will be created " )
+					db = true
+				}
+		}
+		
+		return db
+	}
+	
+	def getArrowheadDB()
+	{
+		return input.ArrowHead.DB.arrowHeadDB
+	}
+	
+	def getArrowheadDBLog()
+	{
+		return input.ArrowHead.DB.arrowHeadLogDB
+	}
+	
+	def getImages()
+	{
+		return input.Images
+	}
+	
+	def isDB(image )
+	{
+		if( image.database == true )
+			return true
+		else
+			return false
+	}
+	
+	def generateDBScript(DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW, DB_ARROWHEAD, DB_ARROWHEAD_LOG)
+	{
+		def script = []
+		
+		script.add("CREATE DATABASE IF NOT EXISTS ${DB_ARROWHEAD_LOG};")
+		script.add("CREATE DATABASE IF NOT EXISTS ${DB_ARROWHEAD};")
+		script.add("/* test comment */")
+		script.add("CREATE USER '${DEFAULT_DB_ARROWHEAD_USR}'@'localhost' IDENTIFIED BY '${DEFAULT_DB_ARROWHEAD_PSW}';")
+		script.add("CREATE USER '${DEFAULT_DB_ARROWHEAD_USR}'@'%' IDENTIFIED BY '${DEFAULT_DB_ARROWHEAD_PSW}';")
+
+		script.add("USE ${DB_ARROWHEAD_LOG};")
+		script.add("CREATE TABLE logs ( id int(10) unsigned NOT NULL AUTO_INCREMENT, date datetime NOT NULL, origin varchar(255) COLLATE utf8_unicode_ci NOT NULL, level varchar(10) COLLATE utf8_unicode_ci NOT NULL, message varchar(1000) COLLATE utf8_unicode_ci NOT NULL, PRIMARY KEY (id) ) ENGINE=InnoDB AUTO_INCREMENT=1557 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;")
+
+		script.add("GRANT ALL PRIVILEGES ON ${DB_ARROWHEAD_LOG}.* TO '${DEFAULT_DB_ARROWHEAD_USR}'@'%';")
+		script.add("GRANT ALL PRIVILEGES ON ${DB_ARROWHEAD_LOG}.* TO '${DEFAULT_DB_ARROWHEAD_USR}'@'localhost';")
+
+		script.add("GRANT ALL PRIVILEGES ON ${DB_ARROWHEAD}.* TO '${DEFAULT_DB_ARROWHEAD_USR}'@'%';")
+		script.add("GRANT ALL PRIVILEGES ON ${DB_ARROWHEAD}.* TO '${DEFAULT_DB_ARROWHEAD_USR}'@'localhost';")
+		
+		return script.join("\n")
+	}
+	
+	def getDBScript(image)
+	{
+		return image.initDBScript.initDBScriptInput
+	}
+	
+	def getDBScriptPath(image)
+	{
+		return image.initDBScript.initDBScriptPath
+	}
+	
+	def generateDockerFileDB(image, scriptName, ROOT_PSW)
+	{
+		def lines = []
+		
+		if( ! image )
+		{
+			log.addEntry(Constants.ERROR, Constants.ACTION_CHECK, "DB Image not found" )
+			return ""
+		}
+		lines.add("FROM " + getDBImageName(image))
+		lines.add("COPY ./"+scriptName+" /docker-entrypoint-initdb.d/")
+		lines.add("ENV MYSQL_ROOT_PASSWORD=${ROOT_PSW}")
+		return lines.join("\n")
+	}
+	
+	def getDBImageName(image)
+	{
+		
+		def registry = environment.REPO_URL.split('//')[1]
+		
+		return registry +"/"+ image.buildImage
+
+	}
+	
+	def getPortusImageName(image)
+	{
+		def registry = environment.REPO_URL.split('//')[1]
+		
+		return registry + "/" + getCloudName() + "/" + image.repo
+	}
+	
+	def getPortusTag(image)
+	{
+		def tag = ""
+		
+		tag += JenkinsApi.getBuildTimestamp()
+		
+		tag += "-" + Constants.DEFAULT_CLOUD_TAG + JenkinsApi.getBuildNumber()
+		
+		return tag
+	}
+	
+	def getCloudName()
+	{
+		def cloud = input.NameSpace.cloud
+		
+		if( input.NameSpace.new && input.NameSpace.new == true )
+		{
+			cloud = Constants.DEFAULT_CLOUD_PREFIX + cloud + "-" + PortusApi.getPortusUserName()
+			if( PortusApi.validateNamespace(cloud ) )
+			{
+				def code = PortusApi.validateNamespace(cloud , stdCloudTeam, "Cloud Namespace for User " + PortusApi.getPortusUserName())
+				
+				if( ! code )
+				{
+					log.addEntry(Constants.ERROR, Constants.ACTION_CHECK, "Failed to create cloud " + cloud)
+					return ""
+				}
+			}
+		}
+		
+		return cloud
+	}
+	
+	def getImageName(image)
+	{
+		return image.repo
+	}
+	
+	def getArtifactsPath(image)
+	{
+		return image.artifacts_path
+	}
+	
+	def generateLogProperties(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+	{
+		
+		def lines = []
+		//logger
+		input.Logger.DB.each
+		{
+			key, value ->
+				
+				if( key.equals("log4j.appender.DB.user") )
+				{
+					lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_USR)
+					return
+				}
+				
+				if( key.equals("log4j.appender.DB.password") )
+				{
+					lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_PSW)
+					return
+				}
+				
+				if( isArrowHead4() && key.equals("log4j.appender.DB.driver") )
+					return
+				
+				lines.add(key + "=" + value)
+				
+		}
+		
+		input.Logger.File.each
+		{
+			key, value ->
+			
+				lines.add(key + "=" + value)
+				
+		}
+		
+		input.Logger.General.each
+		{
+			key, value ->
+			
+				lines.add(key + "=" + value)
+				
+		}
+		
+		return lines.join("\n")
+	}
+	
+	def generateAppProperties(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+	{
+		def lines = []
+		//logger
+		for( setting in image.Settings )
+		{
+			//~ lines.add( setting.getValue().getClass() )
+			setting.getValue().each
+			{
+				key, value ->
+			
+					if( ! value && ( value != false ) )
+						return
+					
+					if( key.equals("db_user") )
+					{
+						lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_USR)
+						return
+					}
+					
+					if( key.equals("db_password") )
+					{
+						lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_PSW)
+						return
+					}
+					
+					if( key.equals("db_address") )
+					{
+						lines.add(key + "=" + "jdbc:mysql://" + input.NameSpace.cloud + Constants.DB_ADRESS_SUFFIX + ":3306/"+input.ArrowHead.DB.arrowHeadDB)
+						return
+					}
+					
+					if( key.equals("sr_address") )
+					{
+						lines.add(key + "=" + input.NameSpace.cloud + Constants.SR_ADRESS_SUFFIX )
+						return
+					}
+					
+					lines.add(key + "=" + value)
+			}
+		}
+		
+		return lines.join("\n")
+	}
+	
+	def generateDockerFileArrowHeadService(image)
+	{
+		def lines = []
+		
+		
+		lines.add("FROM " + getDBImageName(image))
+		lines.add("COPY ./target/ " + image.workdir)
+		lines.add("WORKDIR " + image.workdir)
+		if( image.entry_point )
+			lines.add("ENTRYPOINT [\"" + image.entry_point.join("\", \"") + "\" ]")
+		
+		return lines.join("\n")
+		
+	}
+	
+	def checkArrowServiceImageExists()
+	{
+		def found = false
+		
+		input.Images.each
+		{
+			image ->
+				if( image.database == true)
+				{
+					return 
+				}
+				
+				if( image.registry && image.registry == true )
+					found = true
+				
+				if( image.authorization && image.authorization == true )
+					found = true
+				
+				if( image.gateway && image.gateway == true )
+					found = true
+				
+				if( image.eventhandler && image.eventhandler == true )
+					found = true
+				
+				if( image.gatekeeper && image.gatekeeper == true )
+					found = true
+				
+				if( image.orchestrator && image.orchestrator == true )
+					found = true
+				
+				
+		}
+		
+		return found
+		
+	}
+	
+	def getArrowHeadRepo()
+	{
+		return input.ArrowHead.Repo.git
+	}
+	
+	def generateAppProperties4(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+	{
+		def lines = []
+		//logger
+		input.Logger.DB.each
+		{
+			key, value ->
+				
+				if( key.equals("log4j.appender.DB.user") )
+				{
+					lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_USR)
+					return
+				}
+				
+				if( key.equals("log4j.appender.DB.password") )
+				{
+					lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_PSW)
+					return
+				}
+				
+				lines.add(key + "=" + value)
+				
+		}
+		
+		input.Logger.File.each
+		{
+			key, value ->
+			
+				lines.add(key + "=" + value)
+				
+		}
+		
+		input.Logger.General.each
+		{
+			key, value ->
+			
+				lines.add(key + "=" + value)
+				
+		}
+		
+		for( setting in image.Settings )
+		{
+			//~ lines.add( setting.getValue().getClass() )
+			setting.getValue().each
+			{
+				key, value ->
+			
+		
+				if( key.equals("db_user") )
+				{
+					lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_USR)
+					return
+				}
+				
+				if( key.equals("db_password") )
+				{
+					lines.add(key + "=" + DEFAULT_DB_ARROWHEAD_PSW)
+					return
+				}
+				
+				
+				lines.add(key + "=" + value)
+			}
+		}
+		
+		return lines.join("\n")
+	}	
+	
+	def isArrowHead3()
+	{
+		if( input.ArrowHead.Conf.arrowHead3 == true )
+			return true
+		else
+			return false
+	}
+	
+	def isArrowHead4()
+	{
+		if( input.ArrowHead.Conf.arrowHead4 == true )
+			return true
+		else
+			return false
+	}
+}
---
+++ vars/buildArrowHeadServerStackDemoPipeline2.groovy
@@ -0,0 +1,308 @@
+def call( environment, currentBuild, parameter )
+{
+	
+	Constants = new at.pii.jenkins_cpsiot_2018.sandbox.Constants()
+	BuildArrowHeadServerStackHelpers = new at.pii.jenkins_cpsiot_2018.sandbox.buildArrowHeadServerStackHelpers2()
+	JenkinsApi = new at.pii.jenkins_cpsiot_2018.sandbox.JenkinsApi()
+	PortusApi = new at.pii.jenkins_cpsiot_2018.sandbox.PortusApi()
+
+	pipeline
+	{
+		agent any
+		stages
+		{
+		
+			stage("init")
+			{
+				steps
+				{
+					script
+					{
+						withFolderProperties
+						{
+							withCredentials([usernamePassword(credentialsId: environment.PORTUS_USER_TOKEN_API, usernameVariable: 'USER', passwordVariable: 'TOKEN2')])
+							{
+								PortusApi.init(environment.REPO_URL, environment.PORTUS_USER, environment.TOKEN2, environment.PORTUS_USER_ID, Constants)
+								
+								if( PortusApi.getLog().errorsOccured() )
+								{
+									error("Failed")
+								}
+								
+								JenkinsApi.init(currentBuild, environment, Constants)
+								
+								if( JenkinsApi.getLog().errorsOccured() )
+								{
+									error("Failed")
+								}
+							
+								BuildArrowHeadServerStackHelpers.init(parameter, Constants, environment, PortusApi, JenkinsApi)
+								
+								if( BuildArrowHeadServerStackHelpers.getLog().errorsOccured() )
+								{
+									error("Failed")
+								}
+							}
+						
+							//println BuildArrowHeadServerStackHelpers.getParameter()
+						}
+					}
+				}
+			}
+			
+			stage("Create Maven Cache")
+			{
+				agent{ label "master"}
+				steps
+				{
+					sh " docker volume create maven-repo "
+				}
+			}
+			
+			
+
+			stage( "Build" )
+			{
+				agent{ label "master"}
+				
+				steps
+				{
+					checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${BuildArrowHeadServerStackHelpers.getArrowHeadRepo()}"]]]
+					
+					
+					script
+					{
+						if( BuildArrowHeadServerStackHelpers.checkArrowServiceImageExists() )
+						{
+							if( ! BuildArrowHeadServerStackHelpers.checkCompileDockerHub() )
+							{
+								withFolderProperties
+								{							
+									docker.withRegistry("${environment.REPO_URL}", "${environment.PORTUS_CREDS_STD}")
+									{
+										withDockerContainer(args: "${BuildArrowHeadServerStackHelpers.getContainerCompileArgs()}", image: "${BuildArrowHeadServerStackHelpers.getDockerCompileImage()}")
+										{
+											sh "${BuildArrowHeadServerStackHelpers.getCompileCommand()}"
+										}
+									}
+								}
+							}
+							else
+							{
+								withDockerContainer(args: "${BuildArrowHeadServerStackHelpers.getContainerCompileArgs()}", image: "${BuildArrowHeadServerStackHelpers.getDockerCompileImage()}")
+								{
+									sh "${BuildArrowHeadServerStackHelpers.getCompileCommand()}"
+								}
+							}
+						}
+					}
+					
+					script
+					{
+						BuildArrowHeadServerStackHelpers.getImages().each
+						{
+							image ->
+							
+								if( !BuildArrowHeadServerStackHelpers.isDB(image ) )
+								{
+									stash name: "artifacts-${BuildArrowHeadServerStackHelpers.getImageName(image)}", includes: "${BuildArrowHeadServerStackHelpers.getArtifactsPath(image)}**"
+								}								
+						}
+					}
+				}
+			}
+
+			/*
+			stage( "Dockerize Selected Images" )
+			{
+				steps
+				{
+					script
+					{
+						withFolderProperties
+						{
+							BuildArrowHeadServerStackHelpers.getImages().each
+							{
+								image ->
+								
+									if( BuildArrowHeadServerStackHelpers.isDB(image ) )
+									{
+										//generate dockerfile
+										withCredentials(
+										[
+											string(credentialsId: env.DB_Root_PWD, variable: 'DB_ROOT_PWD'),
+											usernamePassword(credentialsId: env.ArrowHead_User_Pwd, usernameVariable: 'DEFAULT_DB_ARROWHEAD_USR', passwordVariable: 'DEFAULT_DB_ARROWHEAD_PSW')
+										])
+										{
+											def DB_ARROWHEAD = BuildArrowHeadServerStackHelpers.getArrowheadDB()
+											def DB_ARROWHEAD_LOG = BuildArrowHeadServerStackHelpers.getArrowheadDBLog()
+											if( BuildArrowHeadServerStackHelpers.checkGenerateDBScript(image) )
+											{
+												sh "rm -rf database_scripts_cpsiot"
+												sh "mkdir database_scripts_cpsiot"
+											
+												dir( "database_scripts_cpsiot" )
+												{
+													writeFile file: 'initDB.sql', text: BuildArrowHeadServerStackHelpers.generateDBScript(DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW, DB_ARROWHEAD, DB_ARROWHEAD_LOG)
+													
+													writeFile file: 'Dockerfile', text: BuildArrowHeadServerStackHelpers.generateDockerFileDB(image, 'initDB.sql', DB_ROOT_PWD)
+													
+												}
+											}
+											else
+											{
+												if( BuildArrowHeadServerStackHelpers.checkInputDBScript(image) )
+												{
+													sh "rm -rf database_scripts_cpsiot"
+													sh "mkdir database_scripts_cpsiot"
+												
+													dir( "database_scripts_cpsiot" )
+													{
+														writeFile file: 'initDB.sql', text: BuildArrowHeadServerStackHelpers.getDBScript(image)
+														
+														writeFile file: 'Dockerfile', text: BuildArrowHeadServerStackHelpers.generateDockerFileDB(image, 'initDB.sql', DB_ROOT_PWD)
+														
+													}
+												}
+												if( BuildArrowHeadServerStackHelpers.checkScriptPathDBScript(image) )
+												{
+													sh "rm -rf database_scripts_cpsiot"
+													sh "mkdir database_scripts_cpsiot"
+													
+													unstash "scriptPath-${BuildArrowHeadServerStackHelpers.getImageName(image)}"
+													
+
+													sh "cp ${BuildArrowHeadServerStackHelpers.getDBScriptPath(image)} database_scripts_cpsiot/initDB.sql "
+													dir( "database_scripts_cpsiot" )
+													{
+														writeFile file: 'Dockerfile', text: BuildArrowHeadServerStackHelpers.generateDockerFileDB(image, 'initDB.sql', DB_ROOT_PWD)
+														
+													}
+												}
+												
+											}
+										}
+										
+										def portusImageName = BuildArrowHeadServerStackHelpers.getPortusImageName(image)
+										
+										def portusTag = BuildArrowHeadServerStackHelpers.getPortusTag(image)
+										
+										if( BuildArrowHeadServerStackHelpers.getLog().errorsOccured() )
+										{
+											error("Failed")
+										}
+										
+										println( portusImageName + ":" + portusTag )
+										
+
+										dir( "database_scripts_cpsiot" )
+										{
+											docker.withRegistry("${environment.REPO_URL}", "${environment.PORTUS_CREDS_STD}")
+											{
+												customImage = docker.build(portusImageName)
+												customImage.push(portusTag)
+											}
+										}
+									}
+									else
+									{
+										withCredentials(
+										[
+											string(credentialsId: env.DB_Root_PWD, variable: 'DB_ROOT_PWD'),
+											usernamePassword(credentialsId: env.ArrowHead_User_Pwd, usernameVariable: 'DEFAULT_DB_ARROWHEAD_USR', passwordVariable: 'DEFAULT_DB_ARROWHEAD_PSW')
+										])
+										{
+											unstash "artifacts-${BuildArrowHeadServerStackHelpers.getImageName(image)}"
+											
+											dir( "${BuildArrowHeadServerStackHelpers.getArtifactsPath(image)}/.." )
+											{
+												if( BuildArrowHeadServerStackHelpers.isArrowHead3() )
+												{
+													writeFile file: 'target/config/app.properties', text: BuildArrowHeadServerStackHelpers.generateAppProperties(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+													writeFile file: 'target/config/log4j.properties', text: BuildArrowHeadServerStackHelpers.generateLogProperties(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+													
+												}
+												
+												if( BuildArrowHeadServerStackHelpers.isArrowHead4() )
+												{
+													writeFile file: 'target/default.conf', text: BuildArrowHeadServerStackHelpers.generateAppProperties(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+													writeFile file: 'target/config/log4j.properties', text: BuildArrowHeadServerStackHelpers.generateLogProperties(image, DEFAULT_DB_ARROWHEAD_USR, DEFAULT_DB_ARROWHEAD_PSW)
+												}
+												
+												writeFile file: 'Dockerfile', text: BuildArrowHeadServerStackHelpers.generateDockerFileArrowHeadService(image)
+												
+												//sh "cat target/config/app.properties"
+												//sh "cat target/config/log4j.properties"
+												//sh "cat Dockerfile"
+												
+												def portusImageName = BuildArrowHeadServerStackHelpers.getPortusImageName(image)
+										
+												def portusTag = BuildArrowHeadServerStackHelpers.getPortusTag(image)
+												
+												if( BuildArrowHeadServerStackHelpers.getLog().errorsOccured() )
+												{
+													error("Failed")
+												}
+												
+												println( portusImageName + ":" + portusTag )
+												
+												docker.withRegistry("${environment.REPO_URL}", "${environment.PORTUS_CREDS_STD}")
+												{
+													customImage = docker.build(portusImageName)
+													customImage.push(portusTag)
+												}
+											}
+										}
+										
+									}
+									
+									if( BuildArrowHeadServerStackHelpers.getLog().errorsOccured() )
+									{
+										error("Failed")
+									}
+							}
+						}
+					}
+				}
+			}
+			*/
+		}
+		
+		post
+		{
+			always
+			{
+				echo 'Runs always'
+				script
+				{
+					if( BuildArrowHeadServerStackHelpers.getLog() )
+						println BuildArrowHeadServerStackHelpers.getLog().printLog()
+					
+					if( PortusApi.getLog() ) 
+						println PortusApi.getLog().printLog()
+				}
+			}
+	
+			success
+			{
+				echo 'This will run only if successful'
+			}
+			
+			failure
+			{
+				echo 'This will run only if failed'
+			}
+			
+			unstable
+			{
+				echo 'This will run only if the run was marked as unstable'
+			}
+			
+			changed
+			{
+				echo 'This will run only if the state of the Pipeline has changed'
+				echo 'For example, the Pipeline was previously failing but is now successful'
+			}
+		}
+	}
+}
